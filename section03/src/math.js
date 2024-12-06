51dd9ccb03b61b16fa444232c376aab256b0d317
fish-minkyu <e951219@naver.com>
2024-12-06 23:22:40 +0900

    3.4 Node.js 모듈 시스템 이해하기(2)

--- section03/src/math.js
+++ section03/src/math.js
@@ -17,3 +17,18 @@ function sub(a, b) {
 
 // ES Module
 export {add, sub};
+
+// 함수에 export 키워드를 붙이는 것도 가능
+export function add1(a, b) {
+    return a + b;
+}
+
+export function sub1(a, b) {
+    return a - b;
+}
+
+// export default
+// : math.js 모듈을 대표하는 단 하나의 default 값 설정.
+export default function multiply(a, b) {
+    return a * b;
+}
