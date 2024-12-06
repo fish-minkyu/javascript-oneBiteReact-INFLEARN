26e8b3c048e4ccb9a6cce5f81f58b967d2cf6a0c
fish-minkyu <e951219@naver.com>
2024-12-06 23:38:35 +0900

    3.5 Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,3 +1,5 @@
+// 3.4 Node.js 모듈 시스템 이해하기.
+
 // CJS
 // const moduleData = require("./math");
 // console.log(moduleData); // { add: [Function: add], sub: [Function: sub] }
@@ -17,6 +19,15 @@ import {add, sub} from "./math.js"; // 모듈의 확장자까지 넣어서 impor
 // 경로가 같다면 import문을 합칠 수도 있다.
 import multiply, {add, sub} from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(multiply(1, 2));
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(multiply(1, 2));
+
+
+// 3.5 Node.js 라이브러리 사용하기
+// 라이브러리 import 하기
+// : 라이브러리를 가져올 땐, 모듈과 다르게 from 뒤에 라이브러리 이름만 명시하면 된다.
+import randomColor from "randomcolor";
+
+const color = randomColor();
+console.log(color);
