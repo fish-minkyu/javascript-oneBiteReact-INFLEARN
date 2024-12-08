725689b026e1d562a52878e4f9fc32c312033254
fish-minkyu <e951219@naver.com>
2024-12-08 19:57:00 +0900

    4.3 React App 구동원리 살펴보기

--- section04/src/main.jsx
+++ section04/src/main.jsx
@@ -3,8 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// createRoot
+// : 인수로 전달받은 HTML 요소를 리액트의 루트로, 즉 뿌리로 만들어주는 역할을 한다.
 createRoot(document.getElementById('root')).render(
+  // StrictMode는 무시해도 된다.
   <StrictMode>
+    {/* html 태그를 쓰듯이 <App />이라고 써놓은 것은
+        컴포넌트를 렌더링하는 문법이다. */}
     <App />
   </StrictMode>,
 )
