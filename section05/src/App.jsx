7dbbee069b4841890821dbc28ccb019edc84f870
fish-minkyu <e951219@naver.com>
2024-12-10 23:06:40 +0900

    5.4 Props로 데이터 전달하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -6,17 +6,34 @@ import Header from "./components/Header.jsx";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
 
+import Button from "./components/Button"
+
 // 함수 컴포넌트
 // : 함수로 만든 컴포넌트
 // "함수 이름" 컴포넌트라고 부른다.
 
 // 부모 컴포넌트
 function App() {
+  // spread 연산자 활용
+  const buttonProps = {
+    text: "메일",
+    color: "red",
+    a: 1,
+    b: 2,
+    c: 3,
+  };
+
   return (
     <>
-    <Header />
-    <Main />
-    <Footer />
+      {/* <Button text={"메일"} color={"red"} />  */}
+      <Button {...buttonProps} />
+      <Button text={"카페"} />
+      {/* props는 HTML요소나 React 컴포넌트도 전달 할 수 있다. */}
+      <Button text={"블로그"}>
+        {/* 자식요소로 배치된 div 컴포넌트가 자연스럽게 Button 컴포넌트에 children으로 전달이 된다. */}
+        <div>자식요소</div>
+        <Header />
+      </Button>
     </>
   );
 };
