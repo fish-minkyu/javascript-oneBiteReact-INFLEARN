a9f96e4b3e52ef29165809423a4b6fa6ef324179
fish-minkyu <e951219@naver.com>
2024-12-15 20:45:11 +0900

    5.7 State와 Props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,33 +1,16 @@
 import './App.css';
-import { useState } from 'react';
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
+
 
 // 함수 컴포넌트
 // : 함수로 만든 컴포넌트
 // "함수 이름" 컴포넌트라고 부른다.
-
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-  
   return (
     <>
-    <div>
-      <h1>{light}</h1>
-      <button onClick={() => {
-        setLight(light === "ON" ? "OFF" : "ON");
-      }}>
-        {light === "ON" ? "끄기" : "켜기"}
-      </button>
-    </div>
-    <div>
-      <h1>{count}</h1>
-        <button onClick={() => {
-          setCount(count + 1);
-        }}>
-          +
-        </button>
-    </div>
-
+    <Bulb />
+    <Counter />
     </>
   );
 };
