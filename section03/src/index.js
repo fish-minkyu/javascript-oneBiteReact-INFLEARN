ad50e0eb0c67ed020a917efa12ad9f451f750847
fish-minkyu <e951219@naver.com>
2025-09-09 22:37:46 +0900

    3.4 ES6모듈 사용법

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,12 +1,13 @@
 // 3.4 Node.js 모듈 시스템 이해하기.
 
-// CJS
-// const moduleData = require("./math");
-// console.log(moduleData); // { add: [Function: add], sub: [Function: sub] }
-// console.log(moduleData.add(1, 2));
-// console.log(moduleData.sub(1, 2));
+import{add, sub} from "./math.js"; // 확장자명까지 적어주기
 
-// 객체 구조분해할당으로 모듈 가져오기
-const {add, sub} = require("./math");
 console.log(add(1, 2));
 console.log(sub(1, 2));
+
+// 모듈의 대표를 import할 땐, 중괄호 없이 불러와야 한다.
+import mul from "./math.js"; // 대표로 불러오면 이름을 변경할 수 있다.
+console.log(mul(2, 3))
+
+// 동일한 모듈을 불러올 땐 이렇게 합쳐서 불러올 수 있다.
+import mul, {add, sub} from "./math.js";
--- section03/src/math.js
+++ section03/src/math.js
@@ -1,5 +1,6 @@
 // math 모듈
 
+// 방법1.
 function add(a, b) {
     return a + b;
 }
@@ -8,8 +9,18 @@ function sub(a, b) {
     return a - b;
 }
 
-// CJS(Common JS 시스템)
-module.exports = {
-    add, 
-    sub
+export{add, sub};
+
+// 방법2.
+export function add2(a, b) {
+    return a + b;
+}
+
+export function sub2(a, b) {
+    return a - b;
+}
+
+// default를 설정하면 math 모듈을 대표하는 모듈이 된다.
+export default function multiply(a, b) {
+    return a * b;
 }
