6b1075cebdebd510ec06cc0e6e08f00fec85fc56
fish-minkyu <e951219@naver.com>
2025-09-27 22:48:41 +0900

    3.5 Node.js 라이브러리 사용하기

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,13 +1,7 @@
-// 3.4 Node.js 모듈 시스템 이해하기.
+// 3.5 Node.js 라이브러리 사용하기
 
-import{add, sub} from "./math.js"; // 확장자명까지 적어주기
+// 라이브러니는 from 뒤에 이름만 명시하면 된다.
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-
-// 모듈의 대표를 import할 땐, 중괄호 없이 불러와야 한다.
-import mul from "./math.js"; // 대표로 불러오면 이름을 변경할 수 있다.
-console.log(mul(2, 3))
-
-// 동일한 모듈을 불러올 땐 이렇게 합쳐서 불러올 수 있다.
-import mul, {add, sub} from "./math.js";
+const color = randomColor();
+console.log(color);
