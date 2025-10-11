93465e9f52d59573a8b40078073fb3d5f59c62db
fish-minkyu <e951219@naver.com>
2025-10-11 21:00:19 +0900

    5.1 실습 준비하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,18 +1,13 @@
-import './App.css';
+import './App.css'
 
-import Register from './components/Register';
-
-// 함수 컴포넌트
-// : 함수로 만든 컴포넌트
-// "함수 이름" 컴포넌트라고 부른다.
 function App() {
+
+
   return (
     <>
-      <Register />
+    <h1>안녕 리액트</h1>
     </>
-  );
-};
+  )
+}
 
-export default App;
-// 모든 컴포넌트들의 조상 역할을 하는 App 컴포넌트를 
-// 특별히 모든 컴포넌트들의 뿌리 역할을 한다고 해서 root 컴포넌트라고 부른다.
+export default App
--- section05/src/main.jsx
+++ section05/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-// 관례상 대부분 react개발자들은 root 컴포넌트의 이름은 App으로 설정한다.
 createRoot(document.getElementById('root')).render(
     <App />
-)
+);
