777d666d5a042fe51583e3a5f6a742017ce12f9c
fish-minkyu <e951219@naver.com>
2025-10-12 01:08:12 +0900

    5.2 React 컴포넌트

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,13 +1,17 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header.jsx"; // Vite로 인해 파일 확장자는 안써도 된다.
+import Main from "./components/Main.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-
-
-  return (
-    <>
-    <h1>안녕 리액트</h1>
-    </>
-  )
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+            <h1>안녕 리액트</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
