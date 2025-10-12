9059170e602585fe685a30ffd9985a268af2b61f
fish-minkyu <e951219@naver.com>
2025-10-12 19:30:31 +0900

    5.4 Props로 데이터 전달하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -2,14 +2,23 @@ import "./App.css";
 import Header from "./components/Header.jsx"; // Vite로 인해 파일 확장자는 안써도 된다.
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
+import Button from "./components/Button.jsx";
 
 function App() {
+    const buttonProps = {
+        text: "카페",
+        color: "blue",
+        a: 1,
+        b: 2,
+    };
+
     return (
         <>
-            <Header />
-            <Main />
-            <Footer />
-            <h1>안녕 리액트</h1>
+            <Button text={"메일"} color={"red"} />
+            <Button {...buttonProps} />
+            <Button text={"블로그"}>
+                <div>자식요소</div>
+            </Button>
         </>
     );
 }
