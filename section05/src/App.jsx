cf291949abd722d33dce5b133b6d5b8d44792b32
fish-minkyu <e951219@naver.com>
2025-10-12 19:58:42 +0900

    5.6 State로 상태관리하기

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,24 +1,34 @@
 import "./App.css";
-import Header from "./components/Header.jsx"; // Vite로 인해 파일 확장자는 안써도 된다.
-import Main from "./components/Main.jsx";
-import Footer from "./components/Footer.jsx";
-import Button from "./components/Button.jsx";
+import { useState } from "react";
 
 function App() {
-    const buttonProps = {
-        text: "카페",
-        color: "blue",
-        a: 1,
-        b: 2,
-    };
+    // 배열의 첫번째 요소는 새롭게 생성된 state의 값이다.
+    // 두번째 요소는 함수인데 state값을 변화시키는 함수가 들어있다.
+    const [count, setCount] = useState(0);
+    const [light, setLight] = useState("OFF");
 
     return (
         <>
-            <Button text={"메일"} color={"red"} />
-            <Button {...buttonProps} />
-            <Button text={"블로그"}>
-                <div>자식요소</div>
-            </Button>
+            <div>
+                <h1>{light}</h1>
+                <button
+                    onClick={() => {
+                        setLight(light === "ON" ? "OFF" : "ON");
+                    }}
+                >
+                    {light === "ON" ? "끄기" : "켜기"}
+                </button>
+            </div>
+            <div>
+                <h1>{count}</h1>
+                <button
+                    onClick={() => {
+                        setCount(count + 1);
+                    }}
+                >
+                    +
+                </button>
+            </div>
         </>
     );
 }
