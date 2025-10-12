92186510e0404a8c3602310b41e53f7550b32c4e
fish-minkyu <e951219@naver.com>
2025-10-12 23:42:05 +0900

    5.10 useRef로 컴포넌트의 변수 생성하기

--- section05/src/components/Register.jsx
+++ section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 // 간단한 회원가입 폼
 // 1. 이름
@@ -12,8 +12,17 @@ const Register = () => {
         country: "",
         bio: "",
     });
+    const countRef = useRef(0);
+    const inputRef = useRef();
 
     const onChage = (e) => {
+        // useRef 대신 let count = 0; 을 하면 안되나?
+        // useRef를 사용하지 않고 일반 JavaScript 변수를 사용한다면
+        // onChange 이벤트가 발생할 때마다 Register 컴포넌트 함수가 재실행이 되는
+        // 즉, 리렌더링이 되므로 JavaScript 변수도 다시 초기화가 된다.
+        countRef.current++;
+        console.log(countRef.current);
+
         setInput({
             // ...input이 없다면 나머지 속성들의 값은 저장이 되지 않는다.
             ...input,
@@ -23,10 +32,18 @@ const Register = () => {
         });
     };
 
+    const onSubmit = () => {
+        if (input.name === "") {
+            // 이름을 입력하는 DOM 요소 포커스
+            inputRef.current.focus();
+        }
+    };
+
     return (
         <div>
             <div>
                 <input
+                    ref={inputRef}
                     name="name"
                     value={input.name}
                     onChange={onChage}
@@ -54,6 +71,8 @@ const Register = () => {
             <div>
                 <textarea name="bio" value={input.bio} onChange={onChage} />
             </div>
+
+            <button onClick={onSubmit}>제출</button>
         </div>
     );
 };
