b103c773383fb1537263e60fb3580256ffa286fd
fish-minkyu <e951219@naver.com>
2025-10-12 19:41:47 +0900

    5.5 이벤트 처리하기

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -1,10 +1,24 @@
-const Button = ({children, text, color = "black"}) => {
+const Button = ({ text, color, children }) => {
+    // 매개변수로 이벤트 객체를 제공한다.
+    const onClickButton = (e) => {
+        console.log(e);
+        console.log(text);
+    };
 
-    return <button style={{ color: color }}>
-        {text} - {color}
-        {children}
-        </button>;
+    return (
+        <button
+            // onClick같은 것들을 이벤트 핸들러
+            // onClick={() => {
+            //     console.log(text);
+            // }}
+            onClick={onClickButton} // 함수 이름만 이벤트 핸들러로 전해준다.
+            // onMouseEnter={onClickButton}
+            style={{ color: color }}
+        >
+            {text} - {color}
+            {children}
+        </button>
+    );
 };
-
 
 export default Button;
