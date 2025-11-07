2f9adb4672608ddc8663c51d04e24219efeb5613
fish-minkyu <e951219@naver.com>
2025-11-07 18:20:47 +0900

    12.11 Home 페이지 구현하기 2. 기능

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -10,19 +10,19 @@ import Notfound from "./pages/Notfound";
 const mockData = [
     {
         id: 1,
-        createdDate: new Date().getTime(),
+        createdDate: new Date("2025-11-07").getTime(),
         emotionId: 1,
         content: "1번 일기 내용",
     },
     {
         id: 2,
-        createdDate: new Date().getTime(),
+        createdDate: new Date("2025-11-06").getTime(),
         emotionId: 2,
         content: "2번 일기 내용",
     },
     {
         id: 3,
-        createdDate: new Date().getTime(),
+        createdDate: new Date("2025-10-19").getTime(),
         emotionId: 3,
         content: "3번 일기 내용",
     },
@@ -45,8 +45,8 @@ function reducer(state, action) {
     }
 }
 
-const DiaryStateContext = createContext();
-const DiaryDispatchContext = createContext();
+export const DiaryStateContext = createContext();
+export const DiaryDispatchContext = createContext();
 
 function App() {
     const [data, dispatch] = useReducer(reducer, mockData);
