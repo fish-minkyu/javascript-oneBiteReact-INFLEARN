dc15cf86b8ea495ab7815c7084fb4a4241e58ce5
fish-minkyu <e951219@naver.com>
2025-11-07 15:31:35 +0900

    12.9 일기 관리 기능 구현하기2

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useReducer } from "react";
+import { useReducer, useRef, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
@@ -12,43 +12,124 @@ const mockData = [
         id: 1,
         createdDate: new Date().getTime(),
         emotionId: 1,
-        content: "1번 일기 내용"
+        content: "1번 일기 내용",
     },
     {
         id: 2,
         createdDate: new Date().getTime(),
         emotionId: 2,
-        content: "2번 일기 내용"
+        content: "2번 일기 내용",
     },
     {
         id: 3,
         createdDate: new Date().getTime(),
         emotionId: 3,
-        content: "3번 일기 내용"
+        content: "3번 일기 내용",
     },
-]
-
+];
 
 function reducer(state, action) {
-    return state;
+    switch (action.type) {
+        case "CREATE":
+            return [action.data, ...state];
+        case "UPDATE":
+            return state.map((item) =>
+                String(item.id) === String(action.data.id) ? action.data : item
+            );
+        case "DELETE":
+            return state.filter(
+                (item) => String(item.id) !== String(action.id)
+            );
+        default:
+            return state;
+    }
 }
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
     const [data, dispatch] = useReducer(reducer, mockData);
+    const idRef = useRef(4);
+
+    // 새로운 일기 추가
+    const onCreate = (createdDate, emotionId, content) => {
+        dispatch({
+            type: "CREATE",
+            data: {
+                id: idRef.current++,
+                createdDate,
+                emotionId,
+                content,
+            },
+        });
+    };
+
+    // 기존 일기 수정
+    const onUpdate = (id, createdDate, emotionId, content) => {
+        dispatch({
+            type: "UPDATE",
+            data: {
+                id,
+                createdDate,
+                emotionId,
+                content,
+            },
+        });
+    };
+
+    // 기존 일기 삭제
+    const onDelete = (id) => {
+        dispatch({
+            type: "DELETE",
+            id,
+        });
+    };
 
     return (
         <>
+            <button
+                onClick={() => {
+                    onCreate(new Date().getTime(), 1, "Hello");
+                }}
+            >
+                일기 추가 테스트
+            </button>
+            <button
+                onClick={() => {
+                    onUpdate(1, new Date().getTime(), 3, "수정된 일기입니다.");
+                }}
+            >
+                일기 수정 테스트
+            </button>
+            <button
+                onClick={() => {
+                    onDelete(1);
+                }}
+            >
+                일기 삭제 테스트
+            </button>
             {/* 1. Routes 컴포넌트 안에 Route 컴포넌트만 들어갈 수 있다. */}
             {/* 2. Routes 컴포넌트 바깥에 배치된 요소들은 페이지 라우팅과는 관련 없이, 모든 페이지에 다 동일하게 렌더링이 된다. */}
-            <Routes>
-                {/* path란 prop를 통해 위에서 아래로 순차적으로 경로를 확인하고 일치하는 경로가 있으면 해당 요소를 렌더링한다. */}
-                <Route path="/" element={<Home />} />
-                <Route path="/new" element={<New />} />
-                <Route path="/diary/:id" element={<Diary />} />
-                <Route path="/edit/:id" element={<Edit />} />
-                {/* 위에 있는 경로가 모두 아닌 경우 Notfound 페이지를 보여준다. */}
-                <Route path="*" element={<Notfound />} />
-            </Routes>
+            <DiaryStateContext.Provider value={data}>
+                <DiaryDispatchContext.Provider
+                    value={{
+                        onCreate,
+                        onUpdate,
+                        onDelete,
+                    }}
+                >
+                    <Routes>
+                        {/* path란 prop를 통해 위에서 아래로 순차적으로 경로를 확인하고 일치하는 경로가 있으면 해당 요소를 렌더링한다. */}
+                        <Route path="/" element={<Home />} />
+                        <Route path="/new" element={<New />} />
+                        <Route path="/diary/:id" element={<Diary />} />
+                        <Route path="/edit/:id" element={<Edit />} />
+                        {/* 위에 있는 경로가 모두 아닌 경우 Notfound 페이지를 보여준다. */}
+                        <Route path="*" element={<Notfound />} />
+                    </Routes>
+                </DiaryDispatchContext.Provider>
+            </DiaryStateContext.Provider>
         </>
     );
 }
