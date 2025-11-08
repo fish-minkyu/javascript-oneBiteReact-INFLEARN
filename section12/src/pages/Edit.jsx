3edc54e69a263acc9172103530ee179775568b4d
fish-minkyu <e951219@naver.com>
2025-11-08 17:18:39 +0900

    12.15 Edit 페이지 구현하기

--- section12/src/pages/Edit.jsx
+++ section12/src/pages/Edit.jsx
@@ -1,9 +1,68 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate, replace } from "react-router-dom";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useContext, useEffect, useState } from "react";
+import { DiaryDispatchContext, DiaryStateContext } from "../App";
 
 const Edit = () => {
     const params = useParams();
+    const nav = useNavigate();
+    const { onUpdate, onDelete } = useContext(DiaryDispatchContext);
+    const data = useContext(DiaryStateContext);
+    const [currentDiaryItem, setCurDiaryItem] = useState();
 
-    return <div>{params.id}번 일기 수정페이지입니다.</div>;
+    useEffect(() => {
+        const currentDiaryItem = data.find(
+            (item) => String(item.id) === String(params.id)
+        );
+
+        // 해당 일기가 존재하지 않을 경우
+        if (!currentDiaryItem) {
+            window.alert("존재하지 않는 일기입니다.");
+            nav("/", { replace: true });
+        }
+
+        setCurDiaryItem(currentDiaryItem);
+    }, [params.id]);
+
+    const onClickDelete = () => {
+        if (window.confirm("일기를 정말 삭제할까요? 다시 복구되지 않아요!")) {
+            onDelete(params.id);
+            nav("/", { replace: true });
+        }
+    };
+
+    const onSubmit = (input) => {
+        if (window.confirm("일기를 정말 수정할까요?")) {
+            onUpdate(
+                params.id,
+                input.createdDate.getTime(),
+                input.emotionId,
+                input.content
+            );
+            nav("/", { replace: true });
+        }
+    };
+
+    return (
+        <div>
+            <Header
+                title={"일기 수정하기"}
+                leftChild={
+                    <Button onClick={() => nav(-1)} text={"< 뒤로 가기"} />
+                }
+                rightChild={
+                    <Button
+                        onClick={onClickDelete}
+                        text={"삭제하기"}
+                        type={"NEGATIVE"}
+                    />
+                }
+            />
+            <Editor initData={currentDiaryItem} onSubmit={onSubmit} />
+        </div>
+    );
 };
 
 export default Edit;
